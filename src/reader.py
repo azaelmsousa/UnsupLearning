e13b86d5ef7351a2c8f812dd93b3cd279771d066
Azael de Melo e Souza <azaelmsousa@gmail.com>
2018-10-29 20:09:17 -0300

    Adding requirement file, src files and initial experiments

---
+++ src/reader.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import os
+
+
+def readBOW(path='../../data/'):
+
+	print(os.listdir(path))
+
+	data = pd.read_csv('%s/bags.csv'%(path))
+
+	return data
+
+def readWord2Vec(path='../../data/'):
+
+	print(os.listdir(path))
+
+	data = pd.read_csv('%s/word2vec.csv'%(path))
+
+	return data
+
+def readNews(path='../../data/'):
+
+	print(os.listdir(path))
+
+	data = pd.read_csv('%s/health.txt'%(path), sep="|", header=None, skiprows=1)
+	data.columns = ["id", "publish_date", "headline_test"]
+
+	return data
