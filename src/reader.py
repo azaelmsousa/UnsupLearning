1d4aab0ed955fc82c29fcb3437ec2a61740123c5
Waner Miranda <wanermiranda@localhost.localdomain>
2018-11-02 13:02:06 -0300

    forcing the read of files using utf8, instead of latin1 from np old versions

--- src/reader.py
+++ src/reader.py
@@ -27,7 +27,8 @@ def readNews(path='../../data/'):
 
 	print(os.listdir(path))
 
-	data = pd.read_csv('%s/health.txt'%(path), sep="|", header=None, skiprows=1)
+	data = pd.read_csv('%s/health.txt'%(path), sep="|", header=None, skiprows=1, encoding='utf8')
 	data.columns = ["id", "publish_date", "headline_test"]
+	print("Read {} lines".format(data.shape[0]))
 
 	return data
